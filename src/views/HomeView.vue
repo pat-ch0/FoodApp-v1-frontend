bef642bdd3f38babd1c5750d6351455c9ce7a50e
Ismail BASSAOUD <ismail.bassaoud@gmail.com>
2024-01-15 11:29:37 +0100

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HomeComponent msg="Welcome to Your Vue.js App" mehdi="Il veut prendre sa pause"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HomeComponent from '@/components/Home.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HomeComponent
+  }
+}
+</script>
