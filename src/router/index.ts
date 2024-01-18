8aabf22c598b312a54b31172262073848dbdb9b3
Anthony <s35391@stagiaire.aelion.fr>
2024-01-18 13:56:24 +0100

    US1 Storage - Add storage route again

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import ProductDetailView from '../views/ProductDetailView.vue'
 import Product from '@/product/product'
 import WelcomeView from '../views/WelcomeView.vue'
+import StorageView from '../views/StorageView.vue'
 
 const productData  = {
       "product_name": "Thai peanut noodle kit includes stir-fry rice noodles & thai peanut seasoning",
@@ -42,7 +43,12 @@ const routes = [
   {
     path: '/welcome',
     name: 'welcome',
-    component: WelcomeView,
+    component: WelcomeView
+  },
+  {
+    path: '/storage',
+    name: 'storage',
+    component: StorageView
   }
   // {
   //   path: '/about',
