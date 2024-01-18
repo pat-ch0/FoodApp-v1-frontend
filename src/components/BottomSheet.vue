774dd0c97d799c1472001c5140275623ee9f83c8
Azzary <43534730+Azzary@users.noreply.github.com>
2024-01-18 10:44:12 +0100

    Create WelcomeView and ProductDetail (#5)
    Co-authored-by: Remi colin <s35387@stagiaire.aelion.fr>

---
+++ src/components/BottomSheet.vue
@@ -0,0 +1,41 @@
+<template>
+    <transition name="bottom-sheet">
+        <div v-if="isOpen" class="bottom-sheet">
+            <slot></slot>
+        </div>
+    </transition>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+
+@Options({
+    props: {
+        isOpen: Boolean,
+        maxHeight: {
+            type: String,
+            default: '50vh',
+        },
+    },
+})
+export default class BottomSheet extends Vue {
+    isOpen!: boolean;
+}
+</script>
+
+
+<style scoped>
+    .bottom-sheet{
+        background-color: white;
+        z-index: 1000;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        border-top-left-radius: 30px;
+        border-top-right-radius: 30px;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.384);
+    }
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import StorageView from '../views/StorageView.vue'
+import ProductDetailView from '../views/ProductDetailView.vue'
+import Product from '@/product/product'
+import WelcomeView from '../views/WelcomeView.vue'
+
+const productData  = {
+      "product_name": "Thai peanut noodle kit includes stir-fry rice noodles & thai peanut seasoning",
+      "ingredients_text_en": "Noodle: rice, water. seasoning packet: peanut, sugar, hydrolyzed soy protein, green onion, corn maltodextrin, spice (including paprika), citric acid, sea salt, extractives of paprika (color), silicon dioxide (added to make free flowing), yeast extract.",
+      "nutriscore_grade": "a",
+      "image_front_url": "https://images.openfoodfacts.org/images/products/073/762/806/4502/front_en.6.400.jpg",
+      "allergens_tags": ["en:peanuts"],
+      "labels_tags": ["en:no-gluten", "en:vegetarian", "en:vegan"]
+};
+
+const price = 0;
+const carbonFootprint = 0;
+
+const product = new Product(
+  price,
+  productData.product_name,
+  productData.ingredients_text_en.split(', '),
+  productData.nutriscore_grade,
+  productData.image_front_url,
+  carbonFootprint,
+  productData.allergens_tags,
+  productData.labels_tags
+);
 
 const routes = [
   {
@@ -9,10 +34,16 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/storage',
-    name: 'storage',
-    component: StorageView
+    path: '/product',
+    name: 'product',
+    component: ProductDetailView,
+    props: { product: product }
   },
+  {
+    path: '/welcome',
+    name: 'welcome',
+    component: WelcomeView,
+  }
   // {
   //   path: '/about',
   //   name: 'about',
